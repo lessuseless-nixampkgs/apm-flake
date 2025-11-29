52552ef6d3eab04beb96d58f6194d4debe61f4f3
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-11-29 03:50:26 -0600

    feat: Add nodejs_24 to devshell

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -7,6 +7,7 @@
       packages = [
         self'.packages.apm
         pkgs.python3
+        pkgs.nodejs_24 # Added as per user request to update flake to use nodejs_24
       ];
       # Add any other development tools here
       # e.g., pkgs.black, pkgs.isort, pkgs.mypy, pkgs.pytest
